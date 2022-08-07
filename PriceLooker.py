8331c958a521aaea0dfba4622d3227970b842a3b
Matteo Pignataro <matteo.pignataro@tiscali.it>
2022-08-07 10:26:16 +0200

    [PriceLooker] Slight modifies about csv reading timings

--- PriceLooker.py
+++ PriceLooker.py
@@ -4,28 +4,37 @@ import csv
 import time
 import tkinter
 import tkinter.messagebox
+from pathlib import Path
 
 # Create the fake head to make amazon think that we are a legit browser
 head = {
     "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 9.10; rv:69.0) Gecko/20100101 Firefox/68.0'}
 
-# Read the CSV file
-file = open("Links.csv")
-csvreader = csv.reader(file)
-
-# Skip the csv header
-next(csvreader)
-
-# Acquire all the csv data
-rows = []
-for row in csvreader:
-    rows.append(row)
-
 # Create the notify window
 window = tkinter.Tk()
 window.wm_withdraw()
 
+# Wait 10 seconds to establish internet connection
+time.sleep(10)
+
 while True:
+    # Read the CSV file
+    script_location = Path(__file__).absolute().parent
+    file_location = script_location / 'Links.csv'
+    file = file_location.open()
+    csvreader = csv.reader(file)
+
+    # Skip the csv header
+    next(csvreader)
+
+    # Acquire all the csv data
+    rows = []
+    for row in csvreader:
+        rows.append(row)
+
+    # Close the file
+    file.close()
+
     # For every row extract the URL and the price target
     for row in rows:
 
@@ -33,7 +42,12 @@ while True:
         URL = row[0]
         threshold = row[1]
 
-        website = requests.get(URL, headers=head)
+        # In case of no internet connection the application doesn't stop
+        try:
+            website = requests.get(URL, headers=head)
+        except:
+            continue
+
         beautifier = BeautifulSoup(website.content, "html.parser")
         # print(beautifier.prettify())
 

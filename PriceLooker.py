b95fe32a468add6303134b2743114dda8af9a5be
Matteo Pignataro <matteo.pignataro@skywarder.eu>
2023-05-01 20:59:02 +0200

    [Try-Except] expanded try section to avoid price not found events

--- PriceLooker.py
+++ PriceLooker.py
@@ -45,31 +45,31 @@ while True:
         # In case of no internet connection the application doesn't stop
         try:
             website = requests.get(URL, headers=head)
+
+            beautifier = BeautifulSoup(website.content, "html.parser")
+            # print(beautifier.prettify())
+
+            # Find the actual title of the object
+            title = beautifier.find(id="productTitle").get_text()
+
+            # Find the div containing the price
+            prices = beautifier.find(id="corePriceDisplay_desktop_feature_div")
+
+            # Find the classes with the actual price inside
+            price = prices.find_all("span", {"class": "a-offscreen"})[0]
+
+            # Extract the text from the class
+            price = str(price.get_text())
+
+            # At this point we have the text with the object price and now it needs to be extracted in float form
+            price = float(price[0: len(price) - 1].replace(",", "."))
+
+            # Confront the prices
+            if (price < float(threshold)):
+                window.bell()
+                tkinter.messagebox.showinfo(title=title, message=(
+                    "Is below the threshold! \nThe current price is: " + str(price) + "\nThe threshold price is: " + threshold))
         except:
             continue
-
-        beautifier = BeautifulSoup(website.content, "html.parser")
-        # print(beautifier.prettify())
-
-        # Find the actual title of the object
-        title = beautifier.find(id="productTitle").get_text()
-
-        # Find the div containing the price
-        prices = beautifier.find(id="corePriceDisplay_desktop_feature_div")
-
-        # Find the classes with the actual price inside
-        price = prices.find_all("span", {"class": "a-offscreen"})[0]
-
-        # Extract the text from the class
-        price = str(price.get_text())
-
-        # At this point we have the text with the object price and now it needs to be extracted in float form
-        price = float(price[0: len(price) - 1].replace(",", "."))
-
-        # Confront the prices
-        if(price < float(threshold)):
-            window.bell()
-            tkinter.messagebox.showinfo(title=title, message=(
-                "Is below the threshold! \nThe current price is: " + str(price) + "\nThe threshold price is: " + threshold))
     # Sleep for some time
-    time.sleep(3600)
+    time.sleep(600)
